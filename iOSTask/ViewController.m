32228325af0710aced2b3616e7678f49434f017a
Anurag Dixit <anurag_dixit@dell.com>
2015-12-15 22:33:21 +0530

    Initial Commit

---
+++ iOSTask/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSTask
+//
+//  Created by Anurag Dixit on 12/15/15.
+//  Copyright (c) 2015 mobility2. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
